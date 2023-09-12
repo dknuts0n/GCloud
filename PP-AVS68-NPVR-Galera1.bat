03ab8ff074299808a28d866560da7472a4bbe21a
dknuts0n <62902200+dknuts0n@users.noreply.github.com>
2023-09-11 22:17:53 -0600

    Add files via upload

---
+++ PP-AVS68-NPVR-Galera1.bat
@@ -0,0 +1,1 @@
+gcloud compute ssh avs66-prepr-db-bastion-001 --project cto-opus-middleware-np-acce16 --zone northamerica-northeast1-a --ssh-flag="-L 6018:172.18.96.85:3306"
---
+++ PP-AVS68-NPVR-Galera2.bat
@@ -0,0 +1,1 @@
+gcloud compute ssh avs66-prepr-bastion-001 --project cto-opus-middleware-np-acce16 --zone northamerica-northeast1-a --ssh-flag="-L 6019:172.18.96.86:3306"
